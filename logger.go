e234524316711c7632b5aeb1dfc3405da745b6bb
Christopher Cereske <chris@anovaculinary.com>
2016-04-18 11:51:26 -0700

    Change timestamp once again

--- logger.go
+++ logger.go
@@ -175,5 +175,5 @@ func (l *logger) baseData(givenData ...Data) Data {
 }
 
 func currentTimestamp() string {
-	return time.Now().Format(time.RFC3339Nano)
+	return time.Now().Format("2006-01-02T15:04:05.999Z07:00")
 }
